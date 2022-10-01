83a13aae80c30588f859128e755f514aea6b0beb
demono10000 <48636182+demono10000@users.noreply.github.com>
2022-10-01 10:28:09 +0200

    slider added

---
+++ Assets/Scripts/ControlsScript.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControlsScript : MonoBehaviour
+{
+    public Slider timeSlider;
+    public TextMeshProUGUI timeText;
+    public static string startDate = "2019-09-10";
+    public static string endDate = "2019-09-21";
+    // convert date to seconds
+    DateTime dateTimeStart = DateTime.Parse(startDate);
+    DateTime dateTimeEnd = DateTime.Parse(endDate);
+    // Start is called before the first frame update
+    void Start()
+    {
+        // call SliderUpdate when the slider value changes
+        timeSlider.onValueChanged.AddListener(delegate { SliderUpdate(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SliderUpdate()
+    {
+        // read start time in seconds
+        long start = dateTimeStart.Ticks;
+        long end = dateTimeEnd.Ticks;
+        long delta = end - start;
+        long deltaChange = (long)(delta * timeSlider.value);
+        long currentTime = start + deltaChange;
+        DateTime currentDate = new DateTime(currentTime);
+        Debug.Log(currentDate.ToString("dd.MM.yyyy"));
+        timeText.text = currentDate.ToString("dd.MM.yyyy");
+    }
+}
