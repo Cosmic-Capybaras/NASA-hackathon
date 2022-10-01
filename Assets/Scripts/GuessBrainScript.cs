138e3b1f907cc8df056c54252f262a9bb5cf5a9c
demono10000 <48636182+demono10000@users.noreply.github.com>
2022-10-01 23:44:02 +0200

    guess is working

--- Assets/Scripts/GuessBrainScript.cs
+++ Assets/Scripts/GuessBrainScript.cs
@@ -9,21 +9,24 @@ public class GuessBrainScript : MonoBehaviour
 {
     public TextMeshProUGUI timeText;
     public GameObject starObject;
-    public Button category1btn;
-    public Button category2btn;
-    public Button category3btn;
-    public Button category4btn;
-    public Button category5btn;
-    public Button category6btn;
-    public Button category7btn;
-    public Button category8btn;
+    public Button categoryByDraconis;
+    public Button categoryCataclysmic;
+    public Button categoryCepheid;
+    public Button categoryBinary;
+    public Button categoryChemically;
     private int startTime;
     public Vector2 scale;
+    public bool countTime = true;
     // Start is called before the first frame update
     void Start()
     {
         startTime = (int)Time.time*1000;
         GenerateStars(5);
+        categoryByDraconis.GetComponent<Button>().onClick.AddListener(() => { GuessStar("BY Draconis"); });
+        categoryCataclysmic.GetComponent<Button>().onClick.AddListener(() => { GuessStar("Cataclysmic"); });
+        categoryCepheid.GetComponent<Button>().onClick.AddListener(() => { GuessStar("Cepheid"); });
+        categoryBinary.GetComponent<Button>().onClick.AddListener(() => { GuessStar("Binary"); });
+        categoryChemically.GetComponent<Button>().onClick.AddListener(() => { GuessStar("Chemically Peculiar"); });
     }
 
     // Update is called once per frame
@@ -33,6 +36,7 @@ public class GuessBrainScript : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (!countTime) return;
         // calculate time since start
         int time = (int)(Time.time * 1000) - startTime;
         // convert time to minutes and seconds
@@ -84,5 +88,49 @@ public class GuessBrainScript : MonoBehaviour
             }
             star.GetComponent<StarScript>().UpdateBrightness(0);
         }        
+    }
+    public void GuessStar(string category)
+    {
+        // get star with visible description
+        GameObject selectedStar = null;
+        foreach (GameObject star in GameObject.FindGameObjectsWithTag("Star"))
+        {
+            if (star.transform.Find("Description").gameObject.activeSelf)
+            {
+                selectedStar = star;
+                break;
+            }
+        }
+        // check if the star was selected
+        if (selectedStar == null) return;
+        // check if star color is white
+        if (selectedStar.GetComponent<Image>().color.r != 1 || selectedStar.GetComponent<Image>().color.g != 1 || selectedStar.GetComponent<Image>().color.b != 1)
+        {
+            return;
+        }
+        // check if selected category is correst
+        if (selectedStar.GetComponent<StarScript>().category == category)
+        {
+            // set star color to green
+            selectedStar.GetComponent<Image>().color = new Color(0, 1, 0);
+        }
+        else
+        {
+            // set star color to red
+            selectedStar.GetComponent<Image>().color = new Color(1, 0, 0);
+        }
+        // check if there is any white star
+        foreach (GameObject star in GameObject.FindGameObjectsWithTag("Star"))
+        {
+            if (star.GetComponent<Image>().color.r != 1 || star.GetComponent<Image>().color.g != 1 || star.GetComponent<Image>().color.b != 1)
+            {
+                continue;
+            }
+            else
+            {
+                return;
+            }
+        }
+        countTime = false;
     }
 }
