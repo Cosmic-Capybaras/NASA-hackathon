729675a8ddc2426d58d78ca31d4da899a79498ff
demono10000 <48636182+demono10000@users.noreply.github.com>
2022-10-01 22:30:31 +0200

    game modes updates

--- Assets/Scripts/ControlsScript.cs
+++ Assets/Scripts/ControlsScript.cs
@@ -10,16 +10,26 @@ public class ControlsScript : MonoBehaviour
     public Slider timeSlider;
     public TextMeshProUGUI timeText;
     public static string startDate = "2019-09-10";
-    public static string endDate = "2019-10-04";
+    public static string endDate = "2019-10-05";
+    public Button speedSlowBtn;
+    public Button speedNormalBtn;
+    public Button speedFastBtn;
+    public Button speedStopBtn;
+    private float speed = 0f;
     // convert date to seconds
     DateTime dateTimeStart = DateTime.Parse(startDate);
     DateTime dateTimeEnd = DateTime.Parse(endDate);
     // Start is called before the first frame update
     void Start()
     {
+        speedSlowBtn.GetComponent<Button>().onClick.AddListener(() => { ChangeSpeed(2); });
+        speedNormalBtn.GetComponent<Button>().onClick.AddListener(() => { ChangeSpeed(5); });
+        speedFastBtn.GetComponent<Button>().onClick.AddListener(() => { ChangeSpeed(10); });
+        speedStopBtn.GetComponent<Button>().onClick.AddListener(() => { ChangeSpeed(0); });
         // call SliderUpdate when the slider value changes
         timeSlider.onValueChanged.AddListener(delegate { SliderUpdate(); });
-        timeText.text = "00:00 " + dateTimeStart.ToString("dd.MM.yyyy");
+        timeText.text = "Day: 0.0";
+        StartCoroutine(AutoPlay());
     }
 
     // Update is called once per frame
@@ -46,14 +56,28 @@ public class ControlsScript : MonoBehaviour
         else if (hours <= 0.75) hours = 0.5f;
         else hours = 0.75f;
         int starId = (int)(day * 4 + hours * 4);
-        timeText.text = ((int)(hours*24)).ToString("00") + ":00 " + currentDate.ToString("dd.MM.yyyy");
+        //timeText.text = ((int)(hours*24)).ToString("00") + ":00 " + currentDate.ToString("dd.MM.yyyy");
+        timeText.text = "Day: " + day + "."+(int)(hours*100);
         // find all gameobjects with tag "Star"
         GameObject[] stars = GameObject.FindGameObjectsWithTag("Star");
         // loop through all stars
         foreach (GameObject star in stars)
         {
             // update the brightness of the star
-            star.GetComponent<StarScript>().UpdateBrightness((int)(starId)); // temp /4 bo brak danych
+            star.GetComponent<StarScript>().UpdateBrightness((int)(starId));
+        }
+    }
+    void ChangeSpeed(float value)
+    {
+        speed = value;
+    }
+    IEnumerator AutoPlay()
+    {
+        while (true)
+        {
+            if (timeSlider.value + 0.001f * speed > 1) timeSlider.value = 0;
+            else timeSlider.value += 0.001f * speed;
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
