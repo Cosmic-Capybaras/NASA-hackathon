03ffbdc52abb4106b8b7eed99acde353883ff702
demono10000 <48636182+demono10000@users.noreply.github.com>
2022-10-01 14:28:11 +0200

    menu button press animation

---
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class MenuButtonsScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Sprite imagePress;
+    public Sprite imageRelease;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().sprite = imagePress;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().sprite = imageRelease;
+    }
+}
